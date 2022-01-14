f9f872e6017c9c44b07f225efecccdc1d2f67878
james940522 <James940522@gmail.com>
2022-01-14 18:35:27 +0900

    데이터바인딩

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,18 +1,35 @@
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <h4 :style="스타일">{{ products[0] }}</h4>
+      <p>{{ price1 }}만원</p>
+    </div>
+    <div>
+      <h4 :style="스타일">{{ products[1] }}</h4>
+      <p>{{ price2 }}만원</p>
+    </div>
+    <div>
+      <h4 :style="스타일">{{ products[2] }}</h4>
+      <p>{{ price3 }}만원</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      // 데이터 보관함 object 자료로 저장
+      price1: 60,
+      price2: 70,
+      price3: 80,
+      스타일: "color: blue",
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+    };
   },
+  components: {},
 };
 </script>
 
