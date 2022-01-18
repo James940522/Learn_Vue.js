11d9adf36fbca12a8350f9db63d4301436ec26a0
james940522 <James940522@gmail.com>
2022-01-18 23:28:08 +0900

    vue event handler

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <div>
+    <div class="menu">
+      <a v-for="(el, idx) in 메뉴들" :key="idx">{{ el }}</a>
+    </div>
+    <div v-for="(el, idx) in products" :key="idx">
+      <h4 :style="스타일">{{ el }}</h4>
+      <p>{{ price[idx] }}만원</p>
+      <button @click="신고수[idx]++">허위매물신고</button>
+      <span>신고수 : {{ 신고수[idx] }}</span>
+    </div>
+    <!-- <div>
       <h4 :style="스타일">{{ products[0] }}</h4>
       <p>{{ price1 }}만원</p>
     </div>
@@ -12,7 +20,7 @@
     <div>
       <h4 :style="스타일">{{ products[2] }}</h4>
       <p>{{ price3 }}만원</p>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -22,10 +30,10 @@ export default {
   data() {
     return {
       // 데이터 보관함 object 자료로 저장
-      price1: 60,
-      price2: 70,
-      price3: 80,
+      신고수: [0, 0, 0],
+      price: [60, 70, 80],
       스타일: "color: blue",
+      메뉴들: ["Home", "Shop", "About"],
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
     };
   },
@@ -40,6 +48,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
